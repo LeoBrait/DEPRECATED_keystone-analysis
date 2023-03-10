e28dd31fc1318ddaf9ff115185d1bdaf923a0064
Felipe Alexandre Barbosa <felipe.as.barbosa99@gmail.com>
2023-03-10 11:07:58 -0300

    Update name files

--- inputs_preparation.py
+++ inputs_preparation.py
@@ -15,11 +15,11 @@ path = os. getcwd()
 os.chdir(path)
 
 # Read data
-df = pd.read_csv('phyla.general.relative.matrix_2022_08_19.csv')
+df = pd.read_csv('phyla-relative-ab-keystones-validation.csv')
 
 # Split table by habitats in column "Level 3"
-df_split = [g for _, g in df.groupby('Level_3')]
+df_split = [g for _, g in df.groupby('habitat')]
 
 #Saving new dataframes
 for i, split in enumerate(df_split):
-    df_split[i].to_csv(f"{df_split[i]['Level_2'].iloc[0]}." + f"{df_split[i]['Level_3'].iloc[0]}.csv", index=False)
+    df_split[i].to_csv(f"{df_split[i]['Ecosystem'].iloc[0]}." + f"{df_split[i]['habitat'].iloc[0]}.csv", index=False)
