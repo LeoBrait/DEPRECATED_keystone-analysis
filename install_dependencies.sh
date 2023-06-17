eea4eba67408c6557be48e079f16871e2ad15b78
Leonardo Brait <leonardobrait@gmail.com>
2023-06-17 16:00:55 -0300

    added requirements

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-# Running pip3 to install dependencies for the main program
-python3 -m pip install -r requirements.txt
+conda config --add channels bioconda
+conda config --add channels conda-forge
+conda create --name keystones --file requirements.txt
