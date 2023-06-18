725165e7e0737bf98cbac2e80a89f06cd6d78108
Leonardo Brait <leonardobrait@gmail.com>
2023-06-18 14:27:49 -0300

    wip

--- Python/main.py
+++ Python/main.py
@@ -205,3 +205,31 @@ for i in files:
     # sexec('./src/heatmap_keystones.py '+level+' 2')
 
 print('\nPost-results execution time: ', (datetime.now()-startTime))
+
+
+# Original: ####################################################################
+# import sys
+# sys.path.append('src')
+# from xonsh_py import mkdir_p, sexec
+
+# # this script aims to execute all analysis that needs more than one Ecossistem/habitat to work
+
+# print("\n\nStarting analysis through environments.\n\n")
+# # steps that uses all the generated data (usually used for analysis among all environments and taxonomic levels)
+# for level in ['phyla']:
+
+#     # create output directory
+#     # mkdir -p @('output/transposed_all_environments/'+level)
+#     mkdir_p(['output/transposed_all_environments/'+level])
+
+#     # run the identification of keystones
+#     print("Concat keystones for %s." % level)
+#     sexec('./src/concat_keystones.py '+level)
+
+#     # Generate the  keystones heatmap (total effect)
+#     print("Heatmap keystones for %s." % level)
+#     sexec('./src/heatmap_keystones.py '+level+' 1')
+
+#     # Generate the  keystones heatmap (indirect effect)
+#     print("Heatmap keystones (indirect) for %s." % level)
+#     sexec('./src/heatmap_keystones.py '+level+' 2')
