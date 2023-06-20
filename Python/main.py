e306ad0533f4b73da87e568a25c0bfa8fb0bb210
Leonardo Brait <leonardobrait@gmail.com>
2023-06-20 17:24:55 -0300

    removed smaples threshold

--- Python/main.py
+++ Python/main.py
@@ -49,38 +49,29 @@ for ecosystem in ecosystems:
     for habitat in habitats:
         sub_subset = subset[subset['habitat'] == habitat]
         filename = f"{data_dir}community_subsets/{ecosystem}.{habitat}.tsv"
+        
         #TODO: remove this when correlation function is fixed to accept tsv files
         filename_tsv = f"{data_dir}community_subsets_raw/{ecosystem}.{habitat}.csv"
         
-        if sub_subset.shape[0] < 12:
-            #print(f"Skipping {ecosystem}.{habitat} because it has"
-                  #"fewer than 12 samples.")
-        #else: #TODO Reactivate if you want a cutoff of 12 samples
-            #drop columns with sum equal to 0 (irrelevant taxa)
-            column_sums = sub_subset.sum()
-            zero_sum_columns = column_sums[column_sums == 0].index
-            sub_subset = sub_subset.drop(zero_sum_columns, axis=1)
-            sub_subset = sub_subset.drop(['habitat', 'ecosystem'], axis=1)
+    
+        column_sums = sub_subset.sum()
+        zero_sum_columns = column_sums[column_sums == 0].index
+        sub_subset = sub_subset.drop(zero_sum_columns, axis=1)
+        sub_subset = sub_subset.drop(['habitat', 'ecosystem'], axis=1)
 
-            #TODO: remove this when correlation function is fixed to accept
-            #transposed tsv files
-            os.makedirs(f'{data_dir}community_subsets_raw/', exist_ok=True)
-            sub_subset.to_csv(filename_tsv, sep=',', index=False)
+        #TODO: remove this when correlation function is fixed to accept
+        #transposed tsv files
+        os.makedirs(f'{data_dir}community_subsets_raw/', exist_ok=True)
+        sub_subset.to_csv(filename_tsv, sep=',', index=False)
 
-            #prepare data for sparcc (transpose and remove metadata columns)
-            os.makedirs(f'{data_dir}community_subsets/', exist_ok=True)
-            sub_subset = sub_subset.transpose().reset_index()
-            sub_subset.columns = sub_subset.iloc[0]
-            sub_subset = sub_subset.drop([0]).reset_index(drop=True).rename(
-                columns={sub_subset.columns[0]: "#OTU ID"})
-
-            sub_subset.to_csv(filename, sep='\t', index=False)
+        #prepare data for sparcc (transpose and remove metadata columns)
+        os.makedirs(f'{data_dir}community_subsets/', exist_ok=True)
+        sub_subset = sub_subset.transpose().reset_index()
+        sub_subset.columns = sub_subset.iloc[0]
+        sub_subset = sub_subset.drop([0]).reset_index(drop=True).rename(
+            columns={sub_subset.columns[0]: "#OTU ID"})
+        sub_subset.to_csv(filename, sep='\t', index=False)
         continue
-#TODO: remove this when karst porous is fixed
-# karst_file = f'{data_dir}community_subsets/groundwater.karst-porous.tsv'
-# karst_file_tsv = f'{data_dir}community_subsets_raw/groundwater.karst-porous.csv'
-# os.remove(karst_file)
-# os.remove(karst_file_tsv)
 
 ############################# Fastspar #########################################
 startTime = datetime.now()
