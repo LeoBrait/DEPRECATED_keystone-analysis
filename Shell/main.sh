f877b120ee6b4255f147f57e276a8fab5a3b2dfa
leonardo_mussismila <leonardobrait@gmail.com>
2023-06-25 16:39:36 +0000

    solving storage problems

--- Shell/main.sh
+++ Shell/main.sh
@@ -54,10 +54,10 @@ mkdir -p "${performance_dir}"
 # Job List *********************************************************************
 for subset_path in "${iterations_test_subsets[@]}"
 do
-    #parse the file name without the .tsv
-    filename=$(basename -- "$subset_path")
-    filename="${filename%.*}"
-    habitat_dir="${performance_dir}/${filename}"
+    #parse names
+    table_name=$(basename -- "$subset_path")
+    habitat_name="${table_name%.*}"
+    habitat_dir="${performance_dir}/${habitat_name}"
 
     for iteration in "${iterations[@]}"
     do
@@ -75,12 +75,12 @@ do
             #check the pre-existence of each output
             if [ -f "${out_cor}" ]; then
                 echo "echo The Sparcc for: " \
-                        "${filename} with ${iteration} iterations," \
+                        "${habitat_name} with ${iteration} iterations," \
                             "seed ${seed} was already done" > general_log.txt
             else                
                 #create the jobs file in jobs folder
                 echo "echo The Sparcc for:" \
-                        " ${filename} with ${iteration} iterations," \
+                        " ${habitat_name} with ${iteration} iterations," \
                             "seed ${seed} is running..."
                 echo "mkdir -p ${habitat_dir}"
                 echo "mkdir -p ${iteration_dir}"
@@ -94,7 +94,7 @@ do
                     "-x $remove "\
                     "-e 0.1 "\
                     "-y > ${log}"
-                echo "echo ${filename} with ${iteration} iterations," \
+                echo "echo ${habitat_name} with ${iteration} iterations," \
                         " and seed ${seed} done!"
                 echo
             fi
@@ -115,8 +115,8 @@ echo "
 ################################################################################
 Start time: $(date "+%Y-%m-%d %H:%M:%S")"
 
-
 conda activate pyshell_biome_keystones
+
 communities_path=data/community_subsets
 tablenames=($(\ls ${communities_path}))
 fastspar_dir=data/fastspar_correlations
@@ -126,26 +126,31 @@ mkdir -p "${fastspar_dir}"
 for tablename in "${tablenames[@]}";
 do
     #parse the file name without the .tsv
-    filename=$(basename -- "$tablename")
-    filename="${filename%.*}"
+    habitat_name="${tablename%.*}"
+    mkdir -p "${fastspar_dir}/${habitat_name}"
 
+    if [ -f "${fastspar_dir}/${habitat_name}/${habitat_name}.cor" ]; then
+        echo "echo The Sparcc for: ${habitat_name}"\
+        "was already done" > general_log.txt
+    else
     #create the jobs file in jobs folder
-    echo "echo The Sparcc for: ${filename} is running..."
-    echo "mkdir -p ${fastspar_dir}/${filename}"
-    echo "fastspar "\
+        echo "echo The Sparcc for: ${habitat_name} is running..."
+        echo "fastspar "\
             "-c ${communities_path}/${tablename} "\
-            "-r ${fastspar_dir}/${filename}/${filename}.cor "\
-            "-a ${fastspar_dir}/${filename}/${filename}.cov "\
+            "-r ${fastspar_dir}/${habitat_name}/${habitat_name}.cor "\
+            "-a ${fastspar_dir}/${habitat_name}/${habitat_name}.cov "\
             "-t 2 "\
             "-s 1 "\
             "-i $definitive_iter "\
             "-x 15 "\
             "-e 0.1 "\
-            "-y > ${fastspar_dir}/${filename}/${filename}.log"
-    echo "echo ${filename} done!"
-    echo
+            "-y > ${fastspar_dir}/${habitat_name}/${habitat_name}.log"
+        echo "echo ${habitat_name} done!"
+        echo
+    fi
 done > Shell/jobs/fastspar_all_communities.txt
 
+xargs -P $parallel -I {} bash -c "{}" < Shell/jobs/fastspar_all_communities.txt
 
 echo "
 ################################################################################
@@ -153,41 +158,153 @@ echo "
 ################################################################################
 Start time: $(date "+%Y-%m-%d %H:%M:%S")"
 
-# Generate Synthetic habitats
+conda activate pyshell_biome_keystones
+
+#Generate Synthetic habitats
 if [ ! -d "data/synthetic_habitats" ]; then
-    source Shell/pipelines/creating_fake_habitats.sh
-    else
-        echo "Seems that the synthetic habitats are already created.
+
+    # Environment
+    communities_path=data/community_subsets
+    tablenames_real=($(\ls ${communities_path}))
+    general_synthetics_dir=data/synthetic_habitats
+    mkdir -p "${general_synthetics_dir}"
+    mkdir -p "synthetic_habitats_logs"
+
+    # Job List
+    for real_tablename in "${tablenames_real[@]}"; 
+    do
+        #parse the file name without the .tsv
+        habitat="${real_tablename%.*}"
+                     
+        #create the jobs file in jobs folder
+        echo "echo fake_habitat for: ${habitat} is running..."
+        echo "mkdir -p ${general_synthetics_dir}/${habitat}"
+        echo "fastspar_bootstrap" \
+            "-c ${communities_path}/${real_tablename}" \
+            "-n 250" \
+            "-p ${general_synthetics_dir}/${habitat}/synt_" \
+            "-t 2 " \
+            "-s 1 > synthetic_habitats_logs/log_${habitat}.txt"
+        echo "echo fake ${habitat} done!"
+        echo
+    done > Shell/jobs/synthetic_habitats.txt
+
+# Run the jobs
+xargs -P $parallel -I {} bash -c "{}" < Shell/jobs/synthetic_habitats.txt
+
+else
+    echo "Seems that the synthetic habitats are already created.
                 jumping to the next step..."
 fi
 
-# Run fastspar with synthetic communities
-source Shell/pipelines/fastspar_synthetics.sh
+
+
+
+# Run fastspar with synthetic communities **************************************
+# Environment ******************************************************************
+#!/bin/bash
+
+# input
+general_synthetics_dir=data/synthetic_habitats
+synt_habitats_dirs=($(ls ${general_synthetics_dir}))
+
+# output
+mkdir -p data/synthetic_fastspar
+
+for synt_habitat_dir in "${synt_habitats_dirs[@]}";
+do
+    tables=($(ls ${general_synthetics_dir}/${synt_habitat_dir}))
+
+    for table in "${tables[@]}";
+    do
+        #parse the file name without the .tsv
+        filename=$(basename -- "$table")
+        filename="${filename%.*}"
+        habitat=$(basename -- "${synt_habitat_dir}")
+
+        #get table number
+        table_number=$(echo "$filename" | awk -F'_' '{print $NF}')
+
+        #Environment
+        synt_fastspar_dir="data/synthetic_fastspar/${habitat}"
+        remove=15
+        synt_cor="${synt_fastspar_dir}/${habitat}_${table_number}.cor"
+        synt_cov="${synt_fastspar_dir}/${habitat}_${table_number}.cov"
+        log="${synt_fastspar_dir}/log_${habitat}"
+
+    if [ -f "${synt_cor}" ]; then
+        echo "echo The Sparcc for: " \
+                "${habitat} " \
+                   "table ${table_number} was already done" > general_log.txt
+    else         
+        #create the jobs file in jobs folder
+         echo "echo The Sparcc for:" \
+                " synthetic ${habitat}," \
+                    "table ${table_number} is running..."
+        echo "mkdir -p ${synt_fastspar_dir}"
+        echo "fastspar "\
+                     "-c ${general_synthetics_dir}/${synt_habitat_dir}/${table}"\
+                     "-r ${synt_cor}"\
+                     "-a ${synt_cov}"\
+                     "-t 2 "\
+                     "-s 1 "\
+                     "-i 4000 "\
+                     "-x $remove "\
+                     "-e 0.1 "\
+                     "-y > ${log}_${table_number}.txt"
+                 echo "echo table ${table_number} of ${filename}" \
+                         " done!"
+                 echo       
+        fi
+    done
+done > Shell/jobs/fake_fastspar.txt
+
+
+
+# Run the jobs *****************************************************************
+xargs -P $parallel -I {} bash -c "{}" < Shell/jobs/fake_fastspar.txt
+
+
+
 
 # # Test real correlations vs. fake ********************************************
 
-# Environment
-# communities_path=data/community_subsets
-# tablenames_real=($(\ls ${communities_path}))
-# p_values_dir=data/fastspar_pvalues
-# mkdir -p "${p_values_dir}"
+#Environment
 
-# # Job List
-# for tablename_real in "${tablenames_real[@]}";
-# do
-#     #parse the file name without the .tsv
-#     filename=$(basename -- "$tablename_real")
-#     filename="${filename%.*}"
+p_values_dir=data/fastspar_pvalues
+mkdir -p "${p_values_dir}"
 
-#     #create the jobs file in jobs folder
-#     echo "echo The p-values for: ${filename} is running..."
-#     echo "python3 Python/pipelines/fastspar_pvalues.py "\
-#             "-c ${communities_path}/${tablename_real} "\
-#             "-r ${p_values_dir}/${filename}.cor "\
-#             "-n 1000 "\
-#             "-p ${p_values_dir}/${filename}/ "\
-#             "-o ${p_values_dir}/${filename}.tsv "\
-#             "-t 2"
+#real habitats
+communities_path=data/community_subsets
+tablenames_real=($(\ls ${communities_path}))
+
+#synthetic habitats
+general_synthetics_dir=data/synthetic_habitats
+tablepaths_synth=($(\ls ${general_synthetics_dir}))
+
+
+
+# Job List
+for tablename_real in "${tablenames_real[@]}";
+do
+    #parse the file name without the .tsv
+    filename=$(basename -- "$tablename_real")
+    filename="${filename%.*}"
+
+    synt_habitat_dir="${general_synthetics_dir}/${filename}"
+    synt_fastspar_dir="data/synthetic_fastspar/${filename}"
+
+    #create the jobs file in jobs folder
+    echo "echo The p-values for: ${filename} is running..."
+    echo "fastspar_pvalues "\
+            "-c ${communities_path}/${tablename_real} "\
+            "-r ${fastspar_dir}/${filename}/${filename}.cor "\
+            "-p ${synt_habitat_dir}/fake_${filename}"\
+            "-n 250"\
+            "-p ${p_values_dir}/${filename}/ "\
+            "-o ${p_values_dir}/${filename}.tsv "\
+            "-t 2"\
+
 
 
 
