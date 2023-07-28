806222d3330abac451e23ca965214603ad66afac
leonardo_mussismila <leonardobrait@gmail.com>
2023-07-28 20:00:12 +0000

    Modified taxa grouping

--- R/pipelines/veryfiyng_taxa_grouping.R
+++ R/pipelines/veryfiyng_taxa_grouping.R
@@ -19,20 +19,23 @@ install_and_load(
   loc = "r_libs"
 )
 
-
-library("tidyverse")
-library("vegan")
-
 args <- commandArgs(trailingOnly = TRUE)
 analysis_frame <- as.character(args[1])
 annotated_table_relative <- as.character(args[2])
 metadata_table <- as.character(args[3])
 parallel <- as.numeric(args[4])
+minimum_samples <- as.numeric(args[5])
+
+###DEBUG
+# analysis_frame <- "phyla_analysis_july23"
+# annotated_table_relative <- "annotated_table_relative.csv"
+# annotated_table_relative <- "kraken_biomedb_relative_phyla.csv"
+# metadata_table="biome_classification.csv"
+# parallel <- 40
+# minimum_samples <- 5
 
 set.seed(140822)
 options(scipen = 9999999)
-
-
 
 ################################## Load data ###################################
 source("R/src/merge_annotation_metadata.R")
@@ -50,16 +53,15 @@ phyla_abundances <- merge_annotation_metadata(
         "latitude",
         "longitude"))
 
-keystones <- read_csv(paste0(
-    "data/", analysis_frame, "/final_keystones_table/keystones.csv")) %>%
-  rename(habitat = Habitat) %>%
-  mutate(habitat = gsub(" phyla", "", habitat)) %>%
-  mutate(Taxon = gsub("\\.", " ", Taxon))
-
 ############################### Data Treatment #################################
 
 phyla_abundances <- phyla_abundances %>%
-  filter(habitat %in% keystones$habitat)
+  group_by(ecosystem, habitat) %>%
+  mutate(n_samples = n()) %>%
+  filter(n_samples >= minimum_samples) %>%
+  ungroup() %>%
+  select(-n_samples)
+
 
 # Visual treatment
 source("R/src/visual_treat.R")
@@ -125,7 +127,14 @@ if (!file.exists(paste0(results_path, "permanova_lifestyle.RData"))) {
       load(paste0(results_path, "permanova_lifestyle.RData"))
 }
 
+
+cluster <- phyla_abundances %>%
+  group_by(ecosystem, habitat) %>%
+  mutate(cluster = paste0(ecosystem, "_", habitat)) %>%
+  pull(cluster)
+
 if (!file.exists(paste0(results_path, "simper.RData"))) {
+  print("Simper not found!, running...")
   category <- phyla_abundances[["ecosystem"]]
   raw_simper <-  simper(
     standarized_phyla,
@@ -139,3 +148,33 @@ if (!file.exists(paste0(results_path, "simper.RData"))) {
     print("Output already exists!")
     load(paste0(results_path, "simper.RData"))
 }
+
+summary_simper <- summary(raw_simper)
+
+for (name_of_table in names(summary_simper)){
+  df <- summary_simper[[name_of_table]]
+  prev <- 0
+
+  for (rows in 1:nrow(df)){
+    df[rows, 8] <-  df[rows, 6] - prev
+    prev <-  df[rows, 6]
+  }
+
+  df$comparison <- rep(name_of_table, nrow(df))
+  colnames(df) <- c(
+      "average",            "sd",     "ratio",
+          "ava",           "avb",    "cumsum",
+            "p",  "contribution", "comparison"
+  )
+  df <- tibble::rownames_to_column(df, "OTU")
+  simpertables[[name_of_table]] <- df
+}
+
+#Reuniting all data in a single data.frame
+simper_clean <- bind_rows(simpertables)
+simper_clean <- simper_clean[simper_clean$p < 0.05, ]
+
+write.csv(
+  file = "summary_simper.csv",
+  x = simper_clean
+         )
